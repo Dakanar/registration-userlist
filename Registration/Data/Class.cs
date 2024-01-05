174808bebbeceb7be501af14b31b63b4aaaee9a4
Dakanar <155770182+Dakanar@users.noreply.github.com>
2024-01-05 22:48:01 +0300

    Add files via upload

---
+++ Registration/Data/Class.cs
@@ -0,0 +1,10 @@
+﻿namespace Registration.Data
+{
+    public class ApplicationUser
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        // Другие свойства пользователя
+    }
+}
